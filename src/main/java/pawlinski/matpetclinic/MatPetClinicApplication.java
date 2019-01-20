ed17189571d5de2dd74c19ce94e404d19fca278c
mpawlinski <mpawlinski@gmail.com>
2019-01-20 14:13:43 +0100

    Initial Project add

---
+++ src/main/java/pawlinski/matpetclinic/MatPetClinicApplication.java
@@ -0,0 +1,14 @@
+package pawlinski.matpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatPetClinicApplication.class, args);
+	}
+
+}
+
