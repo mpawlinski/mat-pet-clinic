f768a5b5a463f8cb8b0d0e814b032a878733ad30
mpawlinski <mpawlinski@gmail.com>
2019-01-20 19:58:59 +0100

    adding base entity. Closes #10.

--- pet-clinic-data/src/main/java/pawlinski/matpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pawlinski/matpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package pawlinski.matpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/pawlinski/matpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pawlinski/matpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pawlinski.matpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
