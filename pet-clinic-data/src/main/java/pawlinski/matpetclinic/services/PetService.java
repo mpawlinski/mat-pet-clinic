ed264d25f4d3b931b50827dfa3ba352d3b15c981
mpawlinski <mpawlinski@gmail.com>
2019-01-20 19:39:09 +0100

    implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/pawlinski/matpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package pawlinski.matpetclinic.services;
+
+import pawlinski.matpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
