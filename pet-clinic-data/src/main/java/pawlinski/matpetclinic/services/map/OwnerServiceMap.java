1913fa9eb683e06ad561293226cc2051cf54e446
mpawlinski <mpawlinski@gmail.com>
2019-01-21 19:40:13 +0100

    implemented Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/pawlinski/matpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package pawlinski.matpetclinic.services.map;
+
+import pawlinski.matpetclinic.model.Owner;
+import pawlinski.matpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
