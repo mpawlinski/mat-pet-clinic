f49e04e5d966600386299f2b3afa7bd37b10b651
mpawlinski <mpawlinski@gmail.com>
2019-01-28 10:47:07 +0100

    adding Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/pawlinski/matpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package pawlinski.matpetclinic.services;
+
+import pawlinski.matpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/pawlinski/matpetclinic/services/map/SpecialtiesServiceMap.java
@@ -0,0 +1,36 @@
+package pawlinski.matpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pawlinski.matpetclinic.model.Specialty;
+import pawlinski.matpetclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
