a9ba9b14db4f01f5489c107c16573a564e9866cc
mpawlinski <mpawlinski@gmail.com>
2019-02-25 11:45:26 +0100

    added vets JSON endpoint. Closes #63

--- pet-clinic-web/src/main/java/pawlinski/matpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pawlinski/matpetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package pawlinski.matpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pawlinski.matpetclinic.model.Vet;
 import pawlinski.matpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+
+        return vetService.findAll();
     }
 }
